08ca49f98deb1b6c5e7c00b4120ea50585b92812
sidtyagi <mailsiddharthtyagi@gmail.com>
2017-01-02 23:01:18 +0530

    Completed Assignment 1

---
+++ ASSIGNMENT1/assignment1.py
@@ -0,0 +1,32 @@
+'''
+Created on Jan 2, 2017
+
+@author: sidtyagi
+'''
+class MaxSizeList(object):
+    def __init__(self,val):
+        self.max_ele=val
+        self.val=[]
+    def push(self,val):        
+        self.val.append(val)
+    def get_list(self):                
+        return self.val[0:self.max_ele]
+
+
+a=MaxSizeList(3)
+b=MaxSizeList(1)
+
+a.push('hh1')
+a.push('hh2')
+a.push('hh3')
+a.push('hh4')
+a.push('hh5')
+
+b.push('ab1')
+b.push('ab2')
+b.push('ab3')
+b.push('ab4')
+b.push('ab5')
+
+print a.get_list()
+print b.get_list()
